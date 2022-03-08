236ddf3a3a3eaef8bdb84349cd32a371d157a1bf
Armando Montiel <armtiel12@gmail.com>
2022-03-07 19:18:06 -0700

    For Loop Basic I

---
+++ fundamentals/introduction/for_loop_basic1.py
@@ -0,0 +1,33 @@
+for x in range (0, 151):
+    print(x)
+
+print("-" * 25)
+
+for x in range (5, 1001, 5):
+    print(x)
+
+print("-" * 25)
+
+for x in range (1, 101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
+
+sum = 0
+for x in range (0, 500000):
+    if x % 2 == 1:
+        sum = sum + x
+print(sum)
+
+
+for x in range (2018, 1, -4):
+    print(x)
+
+for x in range (2,13):
+    if x % 3 == 0:
+        print(x)
+
+
