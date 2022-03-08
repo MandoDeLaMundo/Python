ff385ae3d4e27d59283c314850170e8e8c31a768
Armando Montiel <armtiel12@gmail.com>
2022-03-07 17:33:42 -0700

    Hello World

---
+++ fundamentals/introduction/hello_world.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+name = "Armando"
+print("Hello", name, "!")
+print("Hello " + name + "!")
+
+name = 9
+print("Hello", name, "!")
+print("Hello " + str(name) + '!')
+
+fave_food1 = "lasagna"
+fave_food2 = "enchiladas"
+
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
