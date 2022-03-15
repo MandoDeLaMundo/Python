82ace831455e6797ffd9c632af45b780c611c43b
Armando Montiel <armtiel12@gmail.com>
2022-03-14 19:58:59 -0600

    Core assignment

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def index():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 1
+    return render_template("index.html")
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+    return redirect('/')
 
 
 if __name__ == "__main__":
